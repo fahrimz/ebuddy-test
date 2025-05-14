3ed6d1a499d80d01252840e6961f9edfdc82fdde
fahrimz <mzfahri620@gmail.com>
2025-05-14 14:08:45 +0700

    feat(be): generate doc for user if didn't exist

--- backend-repo/controller/api.ts
+++ backend-repo/controller/api.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { FirebaseApp } from "firebase/app";
-import { getUserById } from "../repository/userCollection";
+import { createUser, getUserById, setUserRecentlyActive } from "../repository/userCollection";
+import admin from "../config/firebaseConfig";
 
 export const fetchUserData = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -10,10 +10,19 @@ export const fetchUserData = async (req: Request, res: Response): Promise<void> 
       return;
     }
 
-    const userData = await getUserById(userId);
-    if (!userData) {
+    // get user data from auth
+    const firebaseUser = await admin.auth().getUser(userId);
+    if (!firebaseUser) {
       res.status(404).json({ error: 'User not found'});
       return;
+    }
+
+    let userData = await getUserById(userId);
+    if (!userData) { // user exists but don't have doc yet
+      // create initial doc for user
+      userData = await createUser(userId, firebaseUser);
+    } else {
+      await setUserRecentlyActive(userId);
     }
 
     res.status(200).json(userData);
--- backend-repo/repository/userCollection.ts
+++ backend-repo/repository/userCollection.ts
@@ -1,3 +1,4 @@
+import { UserRecord } from "firebase-admin/lib/auth/user-record";
 import { db } from "../config/firebaseConfig";
 import { User } from "../entities/user";
 
@@ -16,3 +17,23 @@ export const getUserById = async (userId: string): Promise<User | null> => {
   }
 }
 
+export const createUser = async (userId: string, firebaseUser: UserRecord): Promise<User> => {
+  const newUser: User = {
+    id: userId,
+    name: firebaseUser.displayName || '',
+    email: firebaseUser.email || '',
+    totalAverageWeightRatings: 0,
+    numberOfRents: 0,
+    recentlyActive: Math.floor(Date.now() / 1000),
+  };
+
+  // Set new user document in Firestore
+  await userCollection.doc(userId).set(newUser);
+  return newUser;
+}
+
+export const setUserRecentlyActive = async (userId: string) => {
+  await userCollection.doc(userId).update({
+    recentlyActive: Math.floor(Date.now() / 1000),
+  });
+}
