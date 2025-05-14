94766a75337c8de893276d87cb16153ee1408911
fahrimz <mzfahri620@gmail.com>
2025-05-14 14:08:59 +0700

    fix(be): wrong token parsing

--- backend-repo/middleware/authMiddleware.ts
+++ backend-repo/middleware/authMiddleware.ts
@@ -3,13 +3,14 @@ import admin from "../config/firebaseConfig";
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = req.headers.authorization?.split(' ').filter(x => x != "Bearer")[0];
+    const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
       res.status(401).json({ error: 'Unauthorized: No Token provided'});
       return;
     }
 
     const decodedToken = await admin.auth().verifyIdToken(token);
+    req.body = req.body || {}; // ensure body exists
     req.body.uid = decodedToken.uid;
     next();
   } catch (err) {
